b3f94c25bbdd765569ac90430824212fda03191e
aaronschanz <56461665+aaronschanz@users.noreply.github.com>
2020-02-24 20:25:21 -0800

    Update Snake.java

--- src/main/java/io/battlesnake/starter/Snake.java
+++ src/main/java/io/battlesnake/starter/Snake.java
@@ -120,6 +120,7 @@ public class Snake {
         public Map<String, String> move(JsonNode moveRequest) {
             Map<String, String> response = new HashMap<>();
             response.put("move", "right");
+            System.out.println("hi");
             return response;
         }
 
